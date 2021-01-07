683e7478d9ce08631d7e3bdf386958699875c1aa
Annapoorni S <annapoornisanjeev@gmail.com>
2021-01-07 18:07:54 +0900

    Recover files.
    Signed-off-by: Annapoorni S <annapoornisanjeev@gmail.com>

---
+++ js/numcount.js
@@ -0,0 +1,23 @@
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+function timeSince() {
+  var prevTime = new Date(2015,8,8,0,0);
+var thisTime = new Date();
+return (thisTime.getTime() - prevTime.getTime()) / 1000;
+}
+
+function parcelCount() {
+  var secDiff = timeSince();
+
+var leftNum = document.getElementById("left");
+var midNum = document.getElementById("mid");
+  var leftNumCount = Math.round(((76/60) * secDiff) + 40093794);
+  var midNumCount = Math.round(((43/60) * secDiff) + 22874098);
+  
+  leftNum.innerHTML = numberWithCommas(leftNumCount);
+  midNum.innerHTML = numberWithCommas(midNumCount);
+}
+parcelCount();
+setInterval(parcelCount, 1000);
---
+++ js/test.js
@@ -0,0 +1,8 @@
+var foods = ['Apples', 'Bananas', 'Oranges']; 1
+for (var i=0; i<foods.length; i++) { 2
+  if (foods[i] == 'Apples') { 3
+    alert(foods[i] + ' are my favorite!'); 4
+  } else {
+    alert(foods[i] + ' are okay.'); 5
+  }
+}
